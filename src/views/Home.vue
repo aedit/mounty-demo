632691cb2f17764dbbc7e73c69988f6c1ccd1860
Udit Sen <udisen97@gmail.com>
2021-06-02 01:44:50 +0530

    Home Page added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1 class="color-primary">Welcome</h1>
+    <h3>to a simple API fetching website based on</h3>
+    <h2>The Movie Database API</h2>
+    <a href="https://themoviedb.org">
+      <img class="home__image" src="@/assets/images/tmdb.svg" alt="" />
+    </a>
+    <ul class="home__links">
+      <li>
+        <router-link to="/movies/top-rated">View Top Rated Movies</router-link>
+      </li>
+      <li>
+        <router-link to="/movies/upcoming">View Upcoming Movies</router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
