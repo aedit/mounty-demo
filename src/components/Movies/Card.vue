fd64804ad53b0eb62e71ce168cf46fb74b2782eb
Udit Sen <udisen97@gmail.com>
2021-06-02 12:06:59 +0530

    Movies page UI changes

---
+++ src/components/Movies/Card.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="movies-results__card">
+    <img
+      class="movies-results__card__image"
+      :src="'https://image.tmdb.org/t/p/w400' + movie.backdrop_path"
+    />
+
+    <h2 class="movies-results__card__title">{{ movie.original_title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- src/components/Movies/Container.vue
+++ src/components/Movies/Container.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="movies-container">
     <div v-if="loading">Loading</div>
-    <div v-else-if="response.results">{{ response.results }}</div>
+    <div class="movies-results" v-else-if="response.results">
+      <Card
+        v-for="(movie, index) in response.results"
+        :key="index"
+        :movie="movie"
+      ></Card>
+    </div>
     <div v-else>Sorry Some Error Occurred</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Card from './Card'
 
 export default {
+  components: { Card },
   data() {
     return {
       api_key: process.env.VUE_APP_API_KEY,
